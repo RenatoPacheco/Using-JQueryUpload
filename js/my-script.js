5aec1f57b7a45ff3c865b7b45ad54a59a55f6ed7
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2014-11-13 20:40:01 -0200

    Adicionada a opção de enviar vários arquivos.

--- js/my-script.js
+++ js/my-script.js
@@ -10,9 +10,19 @@ jQuery(document).ready(function ($) {
         $('form').attr('action', $(this).val());
     });
     $('.arquivo input').bind('change', function (eventData) {
+        var arquivo;
 		$(this).parent().find('label').addClass('on');
-		if($(this).val()) {
-		    $(this).parent().find('label span').text($(this).val());
+		if ($(this).val()) {
+		    try {
+		        arquivo = $(this).val();
+		        if (parseInt($(this).get(0).files.length) > 1) {
+		            arquivo = $(this).get(0).files.length + " arquivo(s) selecionado(s)";
+		        }
+		    } catch(ex)
+		    {
+		        arquivo = $(this).val();
+		    }
+		    $(this).parent().find('label span').text(arquivo);
 		}
 	});
 	$('form').bind('reset', function (eventData) {
